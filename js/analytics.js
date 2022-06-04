a63aa820fc63df289deceaefc7c7fcd21f64bf33
JoBase <hello@jobase.org>
2022-06-04 14:43:59 +0100

    Fixed analytics

--- js/analytics.js
+++ js/analytics.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || []
 function gtag() {dataLayer.push(arguments)}
 
 gtag("js", new Date())
-gtag("config", "G-4QT94Y9S9B", {cookie_flags: "SameSite = None; Secure"})
+gtag("config", "G-DKHQZ28TR3", {cookie_flags: "SameSite=None;Secure"})
